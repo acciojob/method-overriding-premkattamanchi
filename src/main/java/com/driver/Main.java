51bcfde4533241d7637bd923f340449ed3ceca1a
prem <premkattamanchi@gmail.com>
2023-01-17 20:09:31 +0530

    progam written

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String [] args) {
         B obj=new B();
         System.out.println(obj.meth());
-        System.out.println(obj.meth());
     }
 }
 class A{
