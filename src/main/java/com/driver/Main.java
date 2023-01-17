ca448734ee44c1ae76395eab2363f6a5c9b62947
prem <premkattamanchi@gmail.com>
2023-01-17 20:24:32 +0530

    latest

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,18 +1,18 @@
 package com.driver;
 
 public class Main {
+    static class A{
+        public String  meth(){
+            return "Invoking method from class A";
+        }
+    }
+    static class B extends A{
+        public String  meth(){
+            return  "Method is overridden in Extendend class B";
+        }
+    }
     public static void main(String [] args) {
         B obj=new B();
         System.out.println(obj.meth());
-    }
-}
-class A{
-    public String  meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-    public String  meth(){
-        return  "Method is overridden in Extendend class B";
     }
 }
